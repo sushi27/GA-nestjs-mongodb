ea4d30ebef133173edfdba6cfa6cf2f136773ac2
SusantoMandal <86454870+SusantoMandal@users.noreply.github.com>
2023-12-22 14:24:19 +0530

    added .env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,11 +7,24 @@ import { StudentSchema } from './schema/student.schema';
 import { StudentService } from './student/student.service';
 import { StudentController } from './student/student.controller';
 
+const envModule = ConfigModule.forRoot({
+  isGlobal: true,
+});
+
+const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
+const dbUser = process.env.MONGODB_USERNAME;
+const dbPassword = process.env.MONGODB_PASSWORD;
+const dbName = process.env.MONGODB_DB_NAME;
+
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017', {
-      dbName: 'studentdb',
-    }),
+    envModule,
+    MongooseModule.forRoot(
+      `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`,
+      {
+        dbName,
+      },
+    ),
     MongooseModule.forFeature([{ name: 'Student', schema: StudentSchema }]),
   ],
   controllers: [AppController, StudentController],
